c1cfc9491637fb0fed9e3980d481949059196715
Jason Brown <browniefed@gmail.com>
2016-03-20 15:10:45 -0700

    add in selectableness

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,17 +1,104 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
-import styles from './Home.css';
+import { SelectableGroup } from 'react-selectable';
 
 
 export default class Home extends Component {
-  render() {
-    return (
-      <div>
-        <div className={styles.container}>
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
-        </div>
-      </div>
-    );
-  }
+  constructor (props) {
+		super(props);
+		this.state = {
+      boxes: []
+		}
+
+		this.handleSelection = this.handleSelection.bind(this);
+	}
+
+	handleSelection (box) {
+    let boxes = [
+      ...this.state.boxes,
+      box
+    ]
+
+    this.setState({ boxes })
+
+	}
+
+
+	render () {
+		return (
+			<div>
+				<SelectableGroup
+					className="main"
+					style={{position: 'relative'}}
+					ref="selectable"
+					onSelection={this.handleSelection}
+					tolerance={this.state.tolerance}
+					globalMouse={this.state.isGlobal}
+					distance={this.state.distance}>
+          {
+            this.state.boxes.map((box) => {
+
+              const boxStyle = {
+                left: box.boxLeft,
+                top: box.boxTop,
+                width: box.boxWidth,
+                height: box.boxHeight,
+                zIndex: 9000,
+                position: 'absolute',
+                cursor: 'default'
+              };
+
+              const spanStyle = {
+                backgroundColor: 'rgba(49, 150, 245, .2)',
+                width: '100%',
+                height: '100%',
+                float: 'left'
+              };
+
+              return <div style={boxStyle} ref="selectbox"><span style={spanStyle}></span></div>
+            })
+          }
+
+          <img src="http://img.docstoccdn.com/thumb/orig/4510249.png" />
+
+				</SelectableGroup>
+				<SelectableGroup
+					className="main"
+					style={{position: 'relative'}}
+					ref="selectable"
+					onSelection={this.handleSelection}
+					tolerance={this.state.tolerance}
+					globalMouse={this.state.isGlobal}
+					distance={this.state.distance}>
+					{
+						this.state.boxes.map((box) => {
+
+							const boxStyle = {
+								left: box.boxLeft,
+								top: box.boxTop,
+								width: box.boxWidth,
+								height: box.boxHeight,
+								zIndex: 9000,
+								position: 'absolute',
+								cursor: 'default'
+							};
+
+							const spanStyle = {
+								backgroundColor: 'transparent',
+								border: '1px dashed #999',
+								width: '100%',
+								height: '100%',
+								float: 'left'
+							};
+
+							return <div style={boxStyle} ref="selectbox"><span style={spanStyle}></span></div>
+						})
+					}
+
+					<img src="http://img.docstoccdn.com/thumb/orig/4510249.png" />
+
+				</SelectableGroup>
+			</div>
+
+		);
+	}
 }
